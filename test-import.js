14244a96288b9e4844620e28370c805a4298c11f
Ruby <74435331+RubyDevil@users.noreply.github.com>
2021-07-11 15:35:11 -0400

    Update test-import.js

--- test-import.js
+++ test-import.js
@@ -1,8 +1,7 @@
 //var abc = [0, 1, 50, 100, 110, 130, 150, 180, 200, 190];
 
-Array.prototype.add = function(num) { return this.map(val => val + num) };
-Array.prototype.mult = function(num) { return this.map(val => val * num) };
+var $ = window.jQuery;
 
-for(let key of Array.prototype) {
-    Object.defineProperty(Array.prototype, key, {enumerable: false})
+if(typeof custom_background !== 'undefined') {
+    $('.insiderenderpanel').css('background', `url("${custom_background}")`)
 }
