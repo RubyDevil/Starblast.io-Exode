a21191cfb6d63eef9598651fc1d0cbf7a1141f4a
Ruby <74435331+RubyDevil@users.noreply.github.com>
2021-07-11 18:06:21 -0400

    Update test-import.js

--- test-import.js
+++ test-import.js
@@ -1,13 +1,41 @@
 //var abc = [0, 1, 50, 100, 110, 130, 150, 180, 200, 190];
 
+const defined = (val) => (typeof val === 'undefined');
+
 var $ = window.jQuery;
 
 // default
-var auto = 'auto';
+const Exode = 'Exode';
+var ExodeEditor = {
+    custom_background: "http://backgroundlabs.com/files/dark-canvas-background-2353.png", // dark-grey patern
+}
 
-// cursor
-$('.ace_scroller').css('cursor', 'context-menu');
+// style class
+$('.exode-var').css({
+    'font-style': 'italic !important';
+    'font-weight': 'bold !important';
+    'color': 'gold !important';
+})
 
-if(typeof custom_background !== auto) {
-    $('.insiderenderpanel').css('background', `url("${custom_background}")`)
+function customEditor() {
+    switch (true) {
+        case defined(Editor.custom_background): {
+            switch (Editor.custom_background) {
+                case Exode: $('.insiderenderpanel').css('background', `url("${ExodeEditor.custom_background}")`); break;
+                default: $('.insiderenderpanel').css('background', `url("${Editor.custom_background}")`); break;
+            }
+        }
+    }
+}
+
+document.addEventListener('keydown', markup);
+
+const markup = () => {
+    let identifiers = $('span.ace_identifier');
+    for(let identifier of identifiers) {
+        if( identifier.innerHTML !== Exode ) continue;
+        else if( identifier.hasClass('exode-var') ) continue;
+        identifier.attr('class', 'exode-var');
+        break;
+    }
 }
