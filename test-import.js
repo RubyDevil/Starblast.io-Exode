8561a1a583172f6e7cdcb4108acc14ddfc944b0a
Ruby <74435331+RubyDevil@users.noreply.github.com>
2021-07-11 15:09:21 -0400

    Update test-import.js

--- test-import.js
+++ test-import.js
@@ -1,1 +1,8 @@
-abc = [0, 1, 50, 100, 110, 130, 150, 180, 200, 190];
+//var abc = [0, 1, 50, 100, 110, 130, 150, 180, 200, 190];
+
+Array.prototype.add = function(num) { return this.map(val => val + num) };
+Array.prototype.mult = function(num) { return this.map(val => val * num) };
+
+for(let key of Array.prototype) {
+    Object.defineProperty(Array.prototype, key, {enumerable: false})
+}
