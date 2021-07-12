f3b8967f38c26974b2ef16cc2509e77d8207f85a
Ruby <74435331+RubyDevil@users.noreply.github.com>
2021-07-12 13:55:31 -0400

    Update Advanced-Ship-Editor.js

--- Advanced-Ship-Editor.js
+++ Advanced-Ship-Editor.js
@@ -147,10 +147,10 @@ if( typeof Editor !== 'undefined' ) {
     // centerize_canvas  
     if( typeof Editor.centerize_canvas !== 'undefined' && _isNew('centerize_canvas') ) {
         switch (Editor.centerize_canvas) {
-            case 'Exode': $('#insiderenderpanel > canvas').css({ 'width':'unset !important', 'height':'100%' }) ; break;
+            case 'Exode': $('#insiderenderpanel > canvas').css({ 'width':'unset', 'height':'100%' }) ; break;
             default: 
                 ( Editor.centerize_canvas === true )
-                ? $('#insiderenderpanel > canvas').css({ 'width':'unset !important', 'height':'100%' }) 
+                ? $('#insiderenderpanel > canvas').css({ 'width':'unset', 'height':'100%' }) 
                 : $('#insiderenderpanel > canvas').css({ 'width':'100%', 'height':'unset' });
                 break;
         };
