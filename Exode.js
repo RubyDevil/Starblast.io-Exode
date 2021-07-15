7389a7a8e82d1de753ca7dec9cc1b697e8451f1d
Ruby <74435331+RubyDevil@users.noreply.github.com>
2021-07-15 15:44:06 -0400

    Update and rename Advanced-Ship-Editor.js to Exode.js

--- Exode.js
+++ Exode.js
@@ -3,45 +3,31 @@
 var $ = window.jQuery;
 var _$ = window;
 
-let installed = () => (typeof _$.OSMinstalled !== 'undefined' );
+const installed = () => ( typeof _$.OSMinstalled !== 'undefined' );
 
-//const _defined = (val) => (typeof val !== 'undefined');
-var _undef = function (property) { return typeof property === 'undefined' };
-const _deprecated  = (data) => (typeof window[data] === 'undefined' || window[data] !== Editor[data]);
-const _update = (data) => (window[data] = Editor[data]);
-const _check4 = (element) => { if(_undef(_$.css[element])) _$.css[element] = {} };
-function _reload(data) {
+const _undef = (property) => ( typeof property === 'undefined' );
+
+const ExodeLog = (info) => { console.log(`[Exode Manager]: ${info}`) };
+
+function _reload() {
 	var stylesheet = '';
 	if(typeof Object.keys(_$.css) !== 'undefined') {
 		for(let element of Object.keys(_$.css)) {
 			let rules = _$.css[element];
-			
-			stylesheet += (`
-${element} {`)
-			
+            stylesheet += (`\n${element} {`);
 			if(typeof Object.keys(rules) === 'undefined') continue;
 			for(let rule of Object.keys(rules)) {
 				let configuration = rules[rule];
-				
-				stylesheet += (`
-    ${rule}: ${configuration};`)
-				
+				stylesheet += (`\n\t${rule}: ${configuration};`);
 			}
-			
-			stylesheet += (`
-}`)
-			
+			stylesheet += (`\n}`);
 		}
 	}
 	$('#exode-styles').html(stylesheet);
-//$(`<style id="exode-styles">
-//${stylesheet}
-//</style>`).appendTo('head')
-	console.log("[Exode Manager]: Updated Syles")
-}
-const _not = (expression) => (expression === false);
+	ExodeLog("Updated Styles");
+};
 
-// Exode Default
+
 const Exode = 'Exode';
 
 
@@ -54,20 +40,47 @@ if( ! installed() ) {
 // ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 
+
+// Install Plugins ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+/** @Prototype_Extend **/
+if( _undef(_$.PrototypeExtend) ) {
+    $.ajax("https://raw.githubusercontent.com/RubyDevil/Open-Source-Models/main/Prototype-Extend-Plugin.js")
+    .done(data => {
+        eval(data);
+        _$.PrototypeExtend = true;
+        return ExodeLog("Prototype Extend Plugin Loaded");
+    })
+    .fail(e => {
+        return ExodeLog("Error => Prototype Extend Plugin Failed");
+    });
+}
+/** @Open_Source_Models **/
+if( _undef(_$.OpenSourceModels) ) {
+    //...
+    _$.OpenSourceModels = true;
+}
+// ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+
 // Custom Editor Module ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 if( ! _undef(Editor) ) {
 
+    const _deprecated  = (data) => (typeof window[data] === 'undefined' || window[data] !== Editor[data]);
+    const _update = (data) => (window[data] = Editor[data]);
+    const _init = (element) => { if(_undef(_$.css[element])) _$.css[element] = {} };
+
     // custom_background
     if( ! _undef(Editor.custom_background) && ! _deprecated('custom_background') ) {
-        _check4('#insiderenderpanel');
+        _init('#insiderenderpanel');
 		let custom_background = Editor.custom_background
 		let _this = '#insiderenderpanel';
 		if( _undef(_$.background_deleted) ) {
-			_$.css[_this]['background-color'] = 'unset'; /* unset current background-color */
-			_$.css[_this]['background-image'] = 'unset'; /* unset current background-image */
-			_$.css[_this]['background-size'] = 'cover'; /* set background to cover */
-			_$.css[_this]['background-position'] = 'center'; /* set background position to center */
+			_$.css[_this]['background-color'] = 'unset';
+			_$.css[_this]['background-image'] = 'unset';
+			_$.css[_this]['background-size'] = 'cover';
+			_$.css[_this]['background-position'] = 'center';
 			_$.background_deleted = true;
 		}
         switch (custom_background) {
@@ -85,7 +98,7 @@ if( ! _undef(Editor) ) {
 	
 	// text_color
     if( ! _undef(Editor.text_color) && _deprecated('text_color') ) {
-		_check4('.ace_identifier')
+		_init('.ace_identifier')
 		let text_color = Editor.text_color
 		let _this = '.ace_identifier';
         switch (Editor.text_color) {
@@ -98,7 +111,7 @@ if( ! _undef(Editor) ) {
 	
 	// italic
     if( ! _undef(Editor.italic) && _deprecated('italic') ) {
-		_check4('.ace_identifier')
+		_init('.ace_identifier')
 		let _this = '.ace_identifier';
         switch (Editor.italic) {
             case 'Exode': _$.css[_this]['font-style'] = 'italic !important'; break;
@@ -114,7 +127,7 @@ if( ! _undef(Editor) ) {
 	
 	// centerize_canvas
     if( ! _undef(Editor.centerize_canvas) && _deprecated('centerize_canvas') ) {
-		_check4('.insiderenderpanel canvas')
+		_init('.insiderenderpanel canvas')
 		let centerize_canvas = Editor.centerize_canvas;
 		let _this = '.insiderenderpanel canvas';
         switch (centerize_canvas) {
@@ -172,344 +185,3 @@ if( ! _undef(Editor) ) {
 
 // ————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-
-
-
-
-
-
-
-
-
-// editing script below
-
-(function /*Advanced Ship Building Script*/() {
-    
-    var Cords, a, b, baseModel, dataType, ds, h, isArray, isBody, isObject, isWing, l, ofs, p, pos, sc, t, _undef, w, _Array, __save;
-
-    pos = 'position';
-    w = 'width';
-    h = 'height';
-    l = 'length';
-    b = 'bump';
-    sc = 'section_segment';
-    ds = 'doubleside';
-    ofs = 'offset';
-    a = 'angle';
-    t = 'texture';
-    p = 'propeller';
-
-    Cords = ['x', 'y', 'z'];
-
-    _Array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    baseModel = {
-        body: {
-            section_segments: 8,
-            width: _Array,
-            height: _Array,
-            angle: 0,
-            texture: _Array,
-            propeller: false
-        },
-        wing: {
-            doubleside: true,
-            width: _Array,
-            length: _Array,
-            angle: _Array,
-            texture: _Array,
-            bump: null
-        }
-    };
-
-    __save = new Map();
-
-    _undef = function (property) {
-        return typeof property === 'undefined';
-    };
-
-    isObject = function (obj) {
-        return typeof obj === 'object';
-    };
-
-    isArray = function (obj) {
-        return Array.isArray(obj);
-    };
-
-    dataType = function (obj) {
-        if (obj[h] || obj[sc]) {
-            return 'body';
-        } else if (obj[l] || obj[b] || obj[ds]) {
-            return 'wing';
-        }
-    };
-
-    isBody = function (obj) {
-        return dataType(obj) === 'body';
-    };
-
-    isWing = function (obj) {
-        return dataType(obj) === 'wing';
-    };
-
-
-    Array.prototype.swipe = function () {
-        return this.map(function (val) {
-            return val * -1;
-        });
-    };
-
-    Array.prototype.add = function (num) {
-        return this.map(function (val) {
-            return val + num;
-        });
-    };
-
-    Array.prototype.mult = function (num) {
-        return this.map(function (val) {
-            return val * num;
-        });
-    };
-
-    Array.prototype.div = function (num) {
-        return this.map(function (val) {
-            return val / num;
-        });
-    };
-
-    Array.prototype.min = function (num) {
-        return this.map(function (val) {
-            if (val < num) {
-                return (val = num);
-            } else {
-                return val;
-            }
-        });
-    };
-
-    Array.prototype.max = function (num) {
-        return this.map(function (val) {
-            if (val > num) {
-                return (val = num);
-            } else {
-                return val;
-            }
-        });
-    };
-
-    Array.prototype.flat = function () {
-        return this.map(function (val) {
-            return val - val + 1;
-        });
-    };
-
-    Array.prototype.ignore = function (arr) {
-        return this.map(function (val) {
-            if (arr.includes(val)) {
-                return (val = 0);
-            } else {
-                return val;
-            }
-        });
-    };
-
-    Array.prototype.replace = function (x, y) {
-        return this.map(function (val) {
-            if (isArray(x) && x.length === 2) {
-                if (x[0] <= val && val <= x[1]) {
-                    return (val = y);
-                } else {
-                    return val;
-                }
-            } else if (val === x) {
-                return (val = y);
-            } else {
-                return val;
-            }
-        });
-    };
-
-    Array.prototype.cleanTex = function () {
-        this[this.length + 1] = this.length - this.length;
-        return this;
-    };
-
-    Array.prototype.baseTex = function (base) {
-        if (base) {
-            this[this.length + 1] = base;
-            return this;
-        } else {
-            return this;
-        }
-    };
-
-
-    Array.prototype.step = function(step) {
-        var i, _i, _len, _results;
-        _results = [];
-        for ((step > 0 ? (_i = 0, _len = this.length) : _i = this.length - 1); step > 0 ? _i < _len : _i >= 0; _i += step) {
-            i = this[_i];
-            _results.push(i);
-        }
-        return _results;
-    };
-
-    Array.prototype.fill = function(val, len) {
-        var i, _i, _j, _k, _len, _ref, _ref1, _results, _results1, _results2;
-        _ref1 = (len ? (function() {
-            _results1 = [];
-            for (var _j = 0; 0 <= len ? _j < len : _j > len; 0 <= len ? _j++ : _j--){ _results1.push(_j); }
-            return _results1;
-        }).apply(this) : this.length === 1 ? (function() {
-        _results2 = [];
-        for (var _k = 0, _ref = this[0]; 0 <= _ref ? _k < _ref : _k > _ref; 0 <= _ref ? _k++ : _k--){ _results2.push(_k); }
-        return _results2;
-    }).apply(this) : this);
-        _results = [];
-        for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-            i = _ref1[_i];
-            _results.push(val);
-        }
-        return _results;
-    };
-
-    Array.prototype.copy = function(ID) {
-        __save.set(ID, this);
-        return this;
-    };
-
-    Array.prototype.paste = function(ID) {
-        return __save.get(ID);
-	};
-	
-
-	String.prototype.important = function() {
-		return this + " !important"
-	}
-
-
-    Object.prototype.scale = function (ratio) {
-        var _object = this;
-        if( ! _undef(this.offset) ) {
-            _object.offset.x = (_undef(this.offset.x))
-            ? undefined : (this.offset.x * ratio);
-            _object.offset.y = (_undef(this.offset.y))
-            ? undefined : (this.offset.y * ratio);
-            _object.offset.z = (_undef(this.offset.z))
-            ? undefined : (this.offset.z * ratio);
-        }
-        _object.width = (_undef(this.width))
-        ? undefined : (this.width.mult(ratio));
-        if(isBody(this)) {
-            if( ! _undef(this.position) ) {
-                _object.position.x = (_undef(this.position.x))
-                ? undefined : (this.position.x.mult(ratio));
-                _object.position.y = (_undef(this.position.y))
-                ? undefined : (this.position.y.mult(ratio));
-                _object.position.z = (_undef(this.position.z))
-                ? undefined : (this.position.z.mult(ratio));
-            }
-            _object.height = (_undef(this.height))
-            ? undefined : (this.height.mult(ratio));
-        }
-        else if(isWing(this)) {
-            if( ! _undef(this.bump) ) {
-                _object.bump.position = (_undef(this.bump.position))
-                ? undefined : (this.bump.position * ratio);
-                _object.bump.size = (_undef(this.bump.size))
-                ? undefined : (this.bump.size * ratio);
-            }
-            _object.angle = (_undef(this.angle))
-            ? undefined : (this.angle.mult(ratio));
-            _object.position = (_undef(this.position))
-            ? undefined : (this.position.mult(ratio));
-        }
-        return _object;
-    }
-
-    Object.prototype.revert = Object.prototype.reflect = function () {
-        var key, val, _results;
-        _results = [];
-        for (key in this) {
-            val = this[key];
-            _results.push(isObject(val) ? (isArray(val) ? (this[key] = val.reverse()) : key === 'position' ? ((this[key].x = this[key].x.reverse()), (this[key].y = this[key].y.swipe().reverse()), (this[key].z = this[key].z.reverse())) : void 0) : void 0);
-        }
-        return _results;
-    };
-
-    Object.prototype.autoComplete = Object.prototype.auto = function () {
-        var key, val, _i, _j, _len, _len1, _ref, _results;
-        if (_undef(this[ofs])) {
-            this[ofs] = {
-                x: 0,
-                y: 0,
-                z: 0
-            };
-        } else {
-            for (_i = 0, _len = Cords.length; _i < _len; _i++) {
-                key = Cords[_i];
-                if (_undef(this[ofs][key]) || !this[ofs][key]) {
-                    this[ofs][key] = 0;
-                }
-            }
-            return this;
-        }
-        if (_undef(this[pos])) {
-            this[pos] = {
-                x: _Array,
-                y: _Array,
-                z: _Array
-            };
-        } else {
-            if (_undef(this[pos][key]) || !this[pos][key]) {
-                for (_j = 0, _len1 = Cords.length; _j < _len1; _j++) {
-                    key = Cords[_j];
-                    this[pos][key] = _Array;
-                }
-            }
-        }
-        if (_undef(this[key]) || !this[key]) {
-            _ref = baseModel[dataType(this)];
-            _results = [];
-            for (key in _ref) {
-                val = _ref[key];
-                _results.push((this[key] = val));
-            }
-            return _results;
-        }
-    };
-
-    Object.prototype.loadModel = Object.prototype.loadObj = function (model) {
-        var key, val;
-        for (key in model) {
-            val = model[key];
-            if (!this[key]) {
-                this[key] = val;
-            }
-        }
-        return this;
-    };
-
-
-    for(let key of Array.prototype) {
-        Object.defineProperty(Array.prototype, key, {
-            enumerable: false
-        })
-    }
-    
-    for(let key of Object.keys(Object.prototype)) {
-        Object.defineProperty(Object.prototype, key, {
-            enumerable: false
-        })
-    }
-    
-    for(let key of Object.keys(String.prototype)) {
-        Object.defineProperty(String.prototype, key, {
-            enumerable: false
-        })
-    }
-
-}).call(this);
-
-
-_$.OSMinstalled = true
